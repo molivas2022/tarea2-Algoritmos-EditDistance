a1a4d5a07d842fbf3ca5a28f816df8b52b1ac6e8
molivas2022 <molivas2022@inf.udec.cl>
2025-06-02 16:49:04 -0400

    Implementación de métodos en solution.h

--- bottom-up-opt.cpp
+++ bottom-up-opt.cpp
@@ -4,13 +4,42 @@
  ***************************************************************/
 
 #include "solution.h"
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 static string _name = string("Optimizado");
 
 size_t BottomUpOptimized::operator()(const string& str1, const string& str2) const {
-    return 0;  /* placeholder */
+    /* Usamos DP para determinar LCS */
+
+    /* 
+    Sea la matriz dinámica de tamaño n1+1 x n2+1.
+    Limitamos a guardar a lo más dos filas.
+    */
+    vector<size_t> row(str2.length() + 1);
+    row[str2.length()] = 0;                         /* fuera de indice <=> str1 = "" */
+    vector<size_t> pre_row(str2.length() + 1, 0);
+
+    /* Recorrer desde la última fila a la primera */
+    for (size_t i = str1.length(); i-- > 0;) {
+        /* Recorrer desde la última columna a la primera */
+        for (size_t j = str2.length(); j-- > 0;) {
+            /* Nuevo símbolo en común */
+            if (str1[i] == str2[j]) {
+                row[j] = pre_row[j+1] + 1;
+                continue;
+            }
+            /* Decisión */
+            row[j] = max(pre_row[j], row[j+1]);
+        }
+        /* Siguiente iteración */
+        pre_row = row;
+    }
+
+    /* row[0] <=> dp[0][0] <=> LCS(str1, str2) */
+    return str1.length() + str2.length() - 2*row[0];
 }
 
 const string& BottomUpOptimized::name() const {
--- recursive.cpp
+++ recursive.cpp
@@ -3,13 +3,27 @@
  ***************************************************************/
 
 #include "solution.h"
+#include <algorithm>
 
 using namespace std;
 
 static string _name = string("Recursivo");
 
+static size_t lcs(const string& str1, const string& str2, size_t i1, size_t i2) {
+    /* Caso base */
+    if (i1 == str1.length() || i2 == str2.length()) {
+        return 0;
+    }
+    /* Nuevo símbolo en común */
+    if (str1[i1] == str2[i2]) {
+        return 1 + lcs(str1, str2, i1 + 1, i2 + 1);
+    }
+    /* Decisión */
+    return max(lcs(str1, str2, i1 + 1, i2), lcs(str1, str2, i1, i2 + 1));
+}
+
 size_t TopDown::operator()(const string& str1, const string& str2) const {
-    return 0;  /* placeholder */
+    return str1.length() + str2.length() - 2*lcs(str1, str2, 0, 0);
 }
 
 const string& TopDown::name() const {
