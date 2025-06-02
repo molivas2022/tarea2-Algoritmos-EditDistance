7ab5b8a6e99c11ddd3c7b7cf6df097c6e1594c52
molivas2022 <molivas2022@inf.udec.cl>
2025-06-02 16:49:33 -0400

    Multiples tests en test.cpp

--- test.cpp
+++ test.cpp
@@ -11,11 +11,13 @@
 using namespace std;
 
 const string chars = "ABCDE";
+const size_t string_length = 12;
+const size_t number_of_tests = 100;
 
 random_device rd;
 mt19937 generator(rd());
 uniform_int_distribution<> char_dist(0, chars.size() - 1);
-uniform_int_distribution<> length_dist(0, 15);
+uniform_int_distribution<> length_dist(0, string_length);
 
 size_t random_length() {
     return length_dist(generator);
@@ -30,7 +32,8 @@ string random_string(size_t length) {
     return r_str;
 }
 
-int main() {
+/* Corresponde a un sólo test aleatorio */
+bool test(const std::vector<Solution*>& solutions, const Solution& verifier) {
     /* Entrada */
     string str1 = random_string(random_length());
     string str2 = random_string(random_length());
@@ -38,25 +41,45 @@ int main() {
     cout << "\tString 1: \t" << '"' << str1 << '"' << endl;
     cout << "\tString 2: \t" << '"' << str2 << '"' << endl;
 
+    /* Verificación */
+    bool success = true;
+    auto ground_truth = verifier(str1, str2);
+
+    /* Salida */
+    cout << "Salida:" << endl;
+    for (auto solution: solutions) {
+        auto output = (*solution)(str1, str2);
+        cout << "\t" << solution->name() << ": \t" << output << endl;
+        if (output != ground_truth) success = false;
+    }
+
+    return success;
+}
+
+int main() {
     /* Métodos */
     std::vector<Solution*>solutions;
     solutions.push_back(new TopDown());
     solutions.push_back(new TopDownMemoization());
     solutions.push_back(new BottomUp());
     solutions.push_back(new BottomUpOptimized());
-    solutions.push_back(new Verifier());
+    
+    Solution* verifier = new Verifier();
 
-    /* Salida */
-    cout << "Salida:" << endl;
-    for (auto solution: solutions) {
-        cout << "\t" << solution->name() << ": \t" << (*solution)(str1, str2) << endl;
-        
+    /* Test */
+    size_t count = 0;
+    for (size_t t = 0; t < number_of_tests; t++) {
+        if (test(solutions, *verifier)) {
+            count++;
+        }
     }
+    cout << endl << "Success rate: " << count << "\\" << number_of_tests << endl;
 
     /* Liberar memoria */
     for (auto solution: solutions) {
         delete solution;
     }
+    delete verifier;
 
     /* Bye-bye */
     return 0;
